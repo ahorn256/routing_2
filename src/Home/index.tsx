465bb74e4e80f4f22117198df8f29a35751d0214
ahorn256 <anton.horn@futuristen.education>
2024-08-30 15:28:28 +0200

    added BrowserRouter

---
+++ src/Home/index.tsx
@@ -0,0 +1,12 @@
+import styles from './index.module.scss';
+
+function Home() {
+  return (
+    <div className={styles.home}>
+      <h2>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo praesentium nostrum similique ipsum, beatae dolore! Cumque dolore saepe neque labore quis sed error, vitae nostrum assumenda sit nemo sint quo!</p>    
+    </div>
+  );
+}
+
+export default Home;
