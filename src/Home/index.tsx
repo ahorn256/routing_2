2dca6774524f2869a682903007b9664209e3d3c5
ahorn256 <anton.horn@futuristen.education>
2024-08-30 15:33:46 +0200

    added Container in Home page

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,11 +1,11 @@
-import styles from './index.module.scss';
+import { Container } from '@mui/material';
 
 function Home() {
   return (
-    <div className={styles.home}>
+    <Container maxWidth='md'>
       <h2>Home</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo praesentium nostrum similique ipsum, beatae dolore! Cumque dolore saepe neque labore quis sed error, vitae nostrum assumenda sit nemo sint quo!</p>    
-    </div>
+    </Container>
   );
 }
 
