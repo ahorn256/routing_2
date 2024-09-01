3a2a2daf8c895fb100ce572206414dd9c3ac9623
ahorn256 <anton.horn@futuristen.education>
2024-09-01 13:39:56 +0200

    added NotFound component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Books from './Books';
 import Home from './Home';
 import Nav from './Nav';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path='/books' element={<Books />}/>
             <Route path='/' element={<Home />}/>
+            <Route path='*' element={<NotFound />}/>
           </Routes>
         </div>
       </BrowserRouter>
